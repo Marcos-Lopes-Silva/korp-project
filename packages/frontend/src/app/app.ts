fdb3101a0666b0504be1b6e4abb16739ce0daa26
Marcos Silva <marcoseduardo_lopes_@hotmail.com>
2026-08-17 21:10:59 -0300

    Sistema emissor de notas fiscais

--- packages/frontend/src/app/app.ts
+++ packages/frontend/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  onSubmit() {
+    console.log('Form submitted:', this.loginForm.value);
+  }
 }
